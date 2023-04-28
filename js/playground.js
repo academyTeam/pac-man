ed09da9099b13f8d46a6e0ffb0f4e261cd0dc77e
Jakob Bender <jakob.bender@mail.de>
2023-04-28 14:22:39 +0200

    init commit

---
+++ js/playground.js
@@ -0,0 +1,21 @@
+
+function myPlaygroundTest() {
+    console.log([
+        ["1", "1", "1"],
+        ["1", "1"],
+        ["1", "1", "1", "1"],
+        ["1"]
+    ]);
+}
+
+function createPlayground() {
+    var playground = [
+        ["1", "1", "1"],
+        ["1", "1"],
+        ["1", "1", "1", "1"],
+        ["1"]
+    ];
+    playground.forEach(line => line.forEach(block => ));
+}
+
+//https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement?retiredLocale=de
