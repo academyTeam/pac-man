5120d27dea31f48cfa9e9d859fd5bfbde9d48439
Jakob Bender <jakob.bender@mail.de>
2023-05-03 15:16:28 +0200

    testing sprite animations map

--- js/test.js
+++ js/test.js
@@ -6,7 +6,9 @@ pacman.src ="../sprites/Pac-Man_SNES.gif";
 pacman.addEventListener("load", loadImage, false);
 
 function loadImage(e) {
+registerKeys();
   animate();
+
 }
 
 var shift = 0;
@@ -14,29 +16,136 @@ var frameWidth =16
 var frameHeight =16;
 var totalFrames = 4;
 var currentFrame = 0;
-var row = 1;
+var row = 0;
 var column = 0;
-var counter = 0;
+var direction = 0;
+
+
+
+
+
 function animate() {
 
+
   context.clearRect(120, 25, 300, 300);
+  context.drawImage(pacman, column+frameWidth, row+frameHeight, frameWidth, frameHeight,
+                                           200, 60, frameWidth, frameHeight);
+                                            column += frameWidth + 0;
 
-  context.drawImage(pacman, column*frameWidth, row*frameHeight, frameWidth, frameHeight,
-                                          200, 60, frameWidth, frameHeight);
+  switch(direction){
+
+  case "Up":
+
+   if (currentFrame == totalFrames) {
+              column = 64;
+              row = 0;
+              currentFrame = 0;
+          }
+
+    break
+
+  case "Left":
+
+   if (currentFrame == totalFrames) {
+              column = 0;
+              row = 0;
+              currentFrame = 0;
+          }
+
+    break
+  case "Down":
+
+  // direction.rotate(90*)
+
+    break
+  case "Right":
+
+   if (currentFrame == totalFrames) {
+              column = 0;
+              row = 0;
+              currentFrame = 0;
+          }
+
+    break
+
+  }
 
 
-      counter++;
-      console.log
 
 
-      column += frameWidth + 0;
 
         if (currentFrame == totalFrames) {
             column = 0;
+            row = 0;
             currentFrame = 0;
         }
 
       currentFrame++;
 
+
+console.log(direction);
+
       requestAnimationFrame(animate);
+
+
 }
+
+  function registerKeys(){
+         addEventListener('keydown', ({key}) => {
+             switch (key) {
+                 case 'w':
+                 case 'ArrowUp':
+
+                   direction = "Up"
+
+
+
+                     break
+                 case 'a':
+                 case 'ArrowLeft':
+
+                 direction = "Left"
+
+
+
+                     break
+                 case 's':
+                 case 'ArrowDown':
+
+                 direction = "Down"
+
+                     break
+                 case 'd':
+                 case 'ArrowRight':
+
+                 direction = "Right"
+
+                     break
+             }
+         });
+
+
+         addEventListener('keyup', ({key}) => {
+             switch (key) {
+                 case 'w':
+                 case 'ArrowUp':
+
+
+                     break
+                 case 'a':
+                 case 'ArrowLeft':
+
+
+                     break
+                 case 's':
+                 case 'ArrowDown':
+
+                 case 'd':
+                 case 'ArrowRight':
+
+                     break
+             }
+         });
+  }
+
+
